f9bbdab6893786d08a33017b12f6cbe6834b56e4
Olcina <karlos.olcina@gmail.com>
2020-02-07 14:12:25 +0100

    log error in app when a bad prediction object is provided

--- app.py
+++ app.py
@@ -54,17 +54,20 @@ def predict():
         """
     
     # Logging the input payload
-    json_payload = request.json
-    LOG.info(f"JSON payload: \n{json_payload}")
-    inference_payload = pd.DataFrame(json_payload)
-    LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
-    # scale the input
-    scaled_payload = scale(inference_payload)
-    # get an output prediction from the pretrained model, clf
-    prediction = list(clf.predict(scaled_payload))
-    # TO DO:  Log the output prediction value
-    LOG.info(f"Prediction value: \n{prediction}")
-    return jsonify({'prediction': prediction})
+    try:
+      json_payload = request.json
+      LOG.info(f"JSON payload: \n{json_payload}")
+      inference_payload = pd.DataFrame(json_payload)
+      LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
+      # scale the input
+      scaled_payload = scale(inference_payload)
+      # get an output prediction from the pretrained model, clf
+      prediction = list(clf.predict(scaled_payload))
+      LOG.info(f"Prediction value: \n{prediction}")
+      return jsonify({'prediction': prediction})
+    except:
+      LOG.info("There was an error on the prediction")
+      return(jsonify({'error'}))
 
 if __name__ == "__main__":
     # load pretrained model as clf
