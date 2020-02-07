80fbf8b01ce761cb14479f4f7978809883f09c72
Olcina <karlos.olcina@gmail.com>
2020-02-07 12:38:30 +0100

    docker file pushed

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def predict():
 if __name__ == "__main__":
     # load pretrained model as clf
     clf = joblib.load("./model_data/boston_housing_prediction.joblib")
-    app.run(host='0.0.0.0', port=8050, debug=True) # specify port=80
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=sounditi/prediction-api .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -p 5001:80 sounditi/prediction-api 
